9a831ad6fc76fb1c4ced9501201c32b0ca84dc99
sergio <elmatadorsergio1234@gmail.com>
2024-11-07 11:20:36 -0400

    recambios

--- administrador/settings.py
+++ administrador/settings.py
@@ -92,6 +92,14 @@ WSGI_APPLICATION = 'administrador.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.postgresql',
+    #    'NAME': 'uab-cis',
+    #    'USER': 'postgres',
+    #    'PASSWORD': 'sercho14',
+    #    'HOST': 'localhost',
+    #    'PORT': '5432'
+    #}
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
